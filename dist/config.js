d0f6fc936124f241e1f7a8ab3177feb46d3f368d
Dmitriy <dmitriy.kolokot@gmail.com>
2024-06-19 16:05:02 +0300

    rc

---
+++ dist/config.js
@@ -0,0 +1,76 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configService = void 0;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+class ConfigService {
+    constructor(env) {
+        this.env = env;
+    }
+    getBrokerUri() {
+        return process.env.BROKER_URI || 'amqp://rabbitmq:rabbitmq@localhost:5672';
+    }
+    getSvc() {
+        return (() => ({
+            AUTH: 'AUTH_SVC',
+            COLLECTOR_GATEWAY_SVC: 'COLLECTOR_GATEWAY_SVC',
+            TRANSACTIONS_MONITORING: 'TRANSACTIONS_MONITORING_SVC',
+            WL_TRANSACTIONS_MONITORING: 'WL_TRANSACTIONS_MONITORING',
+            LAUNCHPAD: 'LAUNCHPAD_SVC',
+            WL: 'WL_SVC',
+            BAF: 'BAF_SVC',
+            LOT: 'LOT_SVC',
+            RATES: 'RATES_SVC',
+            STATIC: 'STATIC_SVC',
+            WL_BAF: 'WL_BAF',
+            'AUTH-BACK-OFFICE': 'AUTH-BACK-OFFICE_SVC',
+            MAILER: 'MAILER_SVC',
+            USERS_ACTIONS_LOGGER: 'USERS_ACTIONS_LOGGER',
+            PAYMENTS: 'PAYMENTS_SVC',
+            CHAINCORE: 'CHAINCORE_SVC',
+            REFRESHES: 'REFRESHES_SVC',
+            MEDIATOR_SVC: 'MEDIATOR_SVC',
+            MEMBERS_SVC: 'MEMBERS_SVC',
+            TRANSLATES_SVC: 'TRANSLATES_SVC',
+            RECAPTCHA_SVC: 'RECAPTCHA_SVC',
+            MESSAGES_MEDIATOR_SVC: 'MESSAGES_MEDIATOR_SVC',
+            IP_INFO_SVC: 'IP_INFO_SVC',
+            VIRTUAL_CARD_SVC: 'VIRTUAL_CARD_SVC',
+            BONUSES_SVC: 'BONUSES_SVC',
+        }))();
+    }
+    getQueue() {
+        return (() => ({
+            auth: process.env.AUTH_QUEUE || 'collector.auth',
+            collector_gateway: process.env.COLLECTOR_GATEWAY_QUEUE || 'collector.gateway',
+            transactions_monitoring: process.env.AUTH_QUEUE || 'collector.transactions.monitoring',
+            wl_transactions_monitoring: process.env.WL_TRANSACTIONS_MONITORING || 'wl.transactions.monitoring',
+            launchpad: process.env.LAUNCHPAD_QUEUE || 'collector.launchpad',
+            static: process.env.STATIC_SVC || 'collector.static',
+            wl: process.env.WL_QUEUE || 'collector.wl',
+            baf: process.env.BAF_QUEUE || 'collector.baf',
+            lot: process.env.LOT_QUEUE || 'collector.lot',
+            rates: process.env.RATES_QUEUE || 'collector.rates',
+            wl_baf: process.env.WL_BAF || 'wl.baf',
+            users_actions_logger: process.env.USERS_ACTIONS_LOGGER || 'ms.users-actions-logger',
+            'auth-back-office': process.env.AUTH_BACK_OFFICE_QUEUE || 'collector.auth-back-office',
+            mailer: process.env.MAILER_QUEUE || 'ms.mailer',
+            payments: process.env.PAYMENTS_QUEUE || 'collector.payments',
+            chaincore: process.env.CHAINCORE_QUEUE || 'collector.chaincore',
+            refreshes: process.env.REFRESHES_QUEUE || 'ms.refreshes',
+            mediator: process.env.MEDIATOR_QUEUE || 'collector.mediator',
+            memebers: process.env.MEMBERS_QUEUE || 'collector.members',
+            translates: process.env.TRANSLATES_QUEUE || 'collector.translates',
+            recaptcha: process.env.RECAPTCHA_QUEUE || 'collector.recaptcha',
+            messages_mediator: process.env.MESSAGES_MEDIATOR_QUEUE || 'collector.messages_mediator',
+            ip_info: process.env.IP_INFO_QUEUE || 'collector.ip_info',
+            virtual_card: process.env.VIRTUAL_CARD_QUEUE || 'collector.virtual_card',
+            bonuses: process.env.BONUSES_QUEUE || 'collector.bonuses',
+        }))();
+    }
+    getEnv() {
+        return this.env || 'development';
+    }
+}
+const configService = new ConfigService(process.env.NODE_ENV);
+exports.configService = configService;
---
+++ dist/index.d.ts
@@ -0,0 +1,127 @@
+export declare const AUTH_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const COLLECTOR_GATEWAY_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const TRANSACTIONS_MONITORING_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const WL_TRANSACTIONS_MONITORING: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const LAUNCHPAD_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const WL_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const BAF_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const LOT_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const RATES_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const STATIC_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const AUTH_BACK_OFFICE_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const WL_BAF: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const REFRESHES_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const MAILER_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const PAYMENTS_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const USERS_ACTIONS_LOGGER_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const CHAINCORE_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const MEDIATOR_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const MEMBERS_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const TRANSLATES_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const RECAPTCHA_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const MESSAGES_MEDIATOR_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const IP_INFO_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const VIRTUAL_CARD_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const BONUSES_SVC: {
+    provide: string;
+    useFactory: () => import("@nestjs/microservices").ClientProxy & import("@nestjs/microservices").Closeable;
+};
+export declare const services: {
+    AUTH: string;
+    COLLECTOR_GATEWAY_SVC: string;
+    TRANSACTIONS_MONITORING: string;
+    WL_TRANSACTIONS_MONITORING: string;
+    LAUNCHPAD: string;
+    WL: string;
+    BAF: string;
+    LOT: string;
+    RATES: string;
+    STATIC: string;
+    WL_BAF: string;
+    'AUTH-BACK-OFFICE': string;
+    MAILER: string;
+    USERS_ACTIONS_LOGGER: string;
+    PAYMENTS: string;
+    CHAINCORE: string;
+    REFRESHES: string;
+    MEDIATOR_SVC: string;
+    MEMBERS_SVC: string;
+    TRANSLATES_SVC: string;
+    RECAPTCHA_SVC: string;
+    MESSAGES_MEDIATOR_SVC: string;
+    IP_INFO_SVC: string;
+    VIRTUAL_CARD_SVC: string;
+    BONUSES_SVC: string;
+};
